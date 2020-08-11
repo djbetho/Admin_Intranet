4d163b467b5d72b53707b5d5d5da6cec84c50512
vagrant <vagrant@homestead>
2020-08-11 19:45:17 +0000

    Revert "new config email"
    This reverts commit 4f5481104605b64d292ddf10b8d97a17b31f9e7d.

--- resources/views/report/vista1.blade.php
+++ resources/views/report/vista1.blade.php
@@ -23,12 +23,7 @@
   <body>
       <h1>Reportes por mes de los permisos aceptados</h1>
     @foreach($lista_pdf as $c)
-
-    @isset($c[0]->rut)
-      {{$c[0]->rut   }} {{$c[0]->user->name}} {{$c[0]->user->apellido}}
-    @endisset
-      {{$c->rut   }} {{$c->user->name}} {{$c->user->apellido}}
-
+      {{$c[0]->rut}} {{$c[0]->user->name}} {{$c[0]->user->apellido}}
      <table class="table">
         <thead>
             <tr>
@@ -41,9 +36,7 @@
         </thead>
 
         <tbody>
-          @isset($c->rut)
           @foreach($c as $a)
-          @isset($a->detalle)
                   <tr>
                   <td> {{$a->detalle}}</td>
                   <td> {{$a->reemplazo}}</td>
@@ -51,17 +44,6 @@
                   <td align="center"> {{$a->cantidad_dias}}</td>
                   <td align="center">{{$a->ss}}</td>
                 </tr>
-           @endisset
-               <tr>
-               <td> {{$c->detalle}}</td>
-               <td> {{$c->reemplazo}}</td>
-               <td> {{$c->fecha_desde}} - {{$c->fecha_hasta}}</td>
-               <td align="center"> {{$c->cantidad_dias}}</td>
-               <td align="center">{{$c->ss}}</td>
-             </tr>
-
-
-
           @endforeach
         </tbody>
     </table>
